d72d0fc20d26ebb230332386b9f7fd7334ce6b49
yu-bin <jncxy@qq.com>
2023-11-05 16:52:35 +0800

    实现让自动填入的内容生效

---
+++ sites/csdn-get-data.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    chrome.runtime.sendMessage('getData', res => {
+        const titleInput = document.querySelector("#txtTitle")
+
+        titleInput.value = res.title
+        // 触发字数校验
+        titleInput.dispatchEvent(new Event('input'))
+
+        document.querySelector("#cke_1_contents > iframe").contentWindow.document.body.innerHTML = res.content
+    })
+}, 5000)
--- sites/csdn.js
+++ sites/csdn.js
@@ -1,12 +1,4 @@
 setTimeout(() => {
-    chrome.runtime.sendMessage('getData', res => {
-        const titleInput = document.querySelector("#txtTitle")
-
-        titleInput.value = res.title
-        // 触发字数校验
-        titleInput.dispatchEvent(new Event('input'))
-
-        const contentInput = document.querySelector("#cke_1_contents > iframe").contentWindow.document.body
-        contentInput.innerHTML = res.content
-    })
-}, 5000)
+    const content = document.querySelector("#cke_1_contents > iframe").contentWindow.document.body.innerHTML
+    window.editorxx.setData(content)
+}, 5500)
